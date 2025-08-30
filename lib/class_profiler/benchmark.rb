936fb408abe3e139d3ba323648965dd37b5c7218
Matthew Greenfield <mattgreenfield1@gmail.com>
2025-08-30 16:08:57 -0700

    Allow benchmarking several types of functions
    - Fix an issue where we were benchmarking methods inside the class-profiler
      namespace as well as methods we shouldn't benchmark (e.g. 'send')
    - Added methods for benchmarking private, public, protected, instance, and
      class methods; also distinguishing between items define in the class or
      inherited/including in the class

--- lib/class_profiler/benchmark.rb
+++ lib/class_profiler/benchmark.rb
@@ -48,16 +48,73 @@ module ClassProfiler
     end
 
     module ClassMethods
+      # Class-level timings for class methods
+      def class_benchmarked
+        @class_benchmarked ||= {}
+      end
+
+      # Helper: exclude helper methods defined by ClassProfiler modules to avoid recursion/noise
+      def helper_owner_module?(owner)
+        owner_name = owner.respond_to?(:name) ? owner.name : nil
+        !!owner_name&.start_with?('ClassProfiler::')
+      end
+
+      # Methods that should never be wrapped on instances to avoid recursion/footguns
+      RESERVED_INSTANCE_METHODS = %i[
+        instance_exec instance_eval send __send__ method public_method
+        respond_to? object_id __id__ class inspect to_s
+      ].freeze
+
+      # Methods that should never be wrapped on the singleton to avoid recursion/footguns
+      RESERVED_SINGLETON_METHODS = %i[
+        send public_send method singleton_method define_singleton_method
+        instance_eval class_eval method_missing respond_to? allocate new superclass
+        inspect to_s name ancestors inherited extend include prepend
+        alias_method remove_method undef_method autoload autoload?
+      ].freeze
+
+      # (internal) Helper: obtain singleton class
+      def singleton_class_of(klass)
+        class << klass; self; end
+      end
+
       ##
       # Benchmarks instance methods and inherited instance methods
-      def benchmark_all_methods
-        benchmark_methods(*instance_methods)
+      #
+      # @param visibility [Symbol] :public, :protected, :private, :all
+      def benchmark_all_methods(visibility: :public)
+        names = case visibility
+                when :public      then public_instance_methods(true)
+                when :protected   then protected_instance_methods(true)
+                when :private     then private_instance_methods(true)
+                when :all         then (instance_methods(true) + protected_instance_methods(true) + private_instance_methods(true)).uniq
+                else public_instance_methods(true)
+                end
+
+        # Exclude helper methods defined by ClassProfiler modules (e.g., #benchmarked)
+        names = names.reject do |m|
+          RESERVED_INSTANCE_METHODS.include?(m) || m.to_s.start_with?('_') || helper_owner_module?(instance_method(m).owner)
+        end
+        benchmark_methods(*names)
       end
 
       ##
       # Benchmarks non-inherited instance methods
-      def benchmark_instance_methods
-        benchmark_methods(*instance_methods(false))
+      #
+      # @param visibility [Symbol] :public, :protected, :private, :all
+      def benchmark_instance_methods(visibility: :public)
+        names = case visibility
+                when :public      then public_instance_methods(false)
+                when :protected   then protected_instance_methods(false)
+                when :private     then private_instance_methods(false)
+                when :all         then (instance_methods(false) + protected_instance_methods(false) + private_instance_methods(false)).uniq
+                else public_instance_methods(false)
+                end
+
+        names = names.reject do |m|
+          RESERVED_INSTANCE_METHODS.include?(m) || m.to_s.start_with?('_') || helper_owner_module?(instance_method(m).owner)
+        end
+        benchmark_methods(*names)
       end
 
       ##
@@ -74,6 +131,63 @@ module ClassProfiler
             result
           end
         end
+      end
+
+      ##
+      # Benchmarks explicit class methods by name
+      def benchmark_class_methods(*method_names)
+        singleton = singleton_class_of(self)
+        method_names.each do |method_name|
+          wrapped_method_name = "_#{method_name}".to_sym
+          singleton.alias_method wrapped_method_name, method_name
+          singleton.define_method(method_name) do |*args, &block|
+            start_time = Time.now
+            result = send(wrapped_method_name, *args, &block)
+            end_time = Time.now
+            class_benchmarked[method_name] = end_time - start_time
+            result
+          end
+        end
+      end
+
+      ##
+      # Benchmarks class methods defined only on this class (non-inherited)
+      #
+      # @param visibility [Symbol] :public, :protected, :private, :all
+      def benchmark_own_class_methods(visibility: :public)
+        singleton = singleton_class_of(self)
+        names = case visibility
+                when :public      then singleton.public_instance_methods(false)
+                when :protected   then singleton.protected_instance_methods(false)
+                when :private     then singleton.private_instance_methods(false)
+                when :all         then (singleton.instance_methods(false) + singleton.protected_instance_methods(false) + singleton.private_instance_methods(false)).uniq
+                else singleton.public_instance_methods(false)
+                end
+
+        names = names.reject do |m|
+          RESERVED_SINGLETON_METHODS.include?(m) || m.to_s.start_with?('_') || helper_owner_module?(singleton.instance_method(m).owner)
+        end
+        benchmark_class_methods(*names)
+      end
+
+      ##
+      # Benchmarks class methods including inherited ones
+      #
+      # @param visibility [Symbol] :public, :protected, :private, :all
+      def benchmark_all_class_methods(visibility: :public)
+        singleton = singleton_class_of(self)
+        names = case visibility
+                when :public      then singleton.public_instance_methods(true)
+                when :protected   then singleton.protected_instance_methods(true)
+                when :private     then singleton.private_instance_methods(true)
+                when :all         then (singleton.instance_methods(true) + singleton.protected_instance_methods(true) + singleton.private_instance_methods(true)).uniq
+                else singleton.public_instance_methods(true)
+                end
+
+        names = names.reject do |m|
+          RESERVED_SINGLETON_METHODS.include?(m) || m.to_s.start_with?('_') || helper_owner_module?(singleton.instance_method(m).owner)
+        end
+        benchmark_class_methods(*names)
       end
     end
   end
--- spec/benchmark_spec.rb
+++ spec/benchmark_spec.rb
@@ -1,42 +1,170 @@
 # frozen_string_literal: true
 
 RSpec.describe ClassProfiler::Benchmark do
-  it 'benchmarks specified methods and records elapsed time' do
-    klass = Class.new do
-      include ClassProfiler::Benchmark
+  let(:benchmarked) { obj.benchmarked }
 
-      def fast = 1.+(1)
-      def slow = sleep(0.002)
+  context 'with benchmark_methods for explicit method list' do
+    let(:klass) do
+      Class.new do
+        include ClassProfiler::Benchmark
 
-      benchmark_methods :fast, :slow
+        def fast = 1.+(1)
+        def slow = sleep(0.002)
+
+        benchmark_methods :fast, :slow
+      end
     end
 
-    obj = klass.new
-    obj.fast
-    obj.slow
+    let(:obj) { klass.new }
 
-    expect(obj.benchmarked).to include(:fast, :slow)
-    expect(obj.benchmarked[:slow]).to be > 0
+    before do
+      obj.fast
+      obj.slow
+    end
+
+    it 'records timings for explicit methods' do
+      expect(benchmarked.keys).to include(:fast, :slow)
+      expect(benchmarked[:fast]).to be_a(Numeric)
+      expect(benchmarked[:slow]).to be_a(Numeric)
+      expect(benchmarked[:fast]).to be >= 0
+      expect(benchmarked[:slow]).to be >= 0
+    end
+
+    it 'measures slow >= fast to guard against flakiness' do
+      expect(benchmarked[:slow]).to be >= benchmarked[:fast]
+    end
   end
 
-  it 'benchmarks only non-inherited instance methods' do
-    parent = Class.new do
-      include ClassProfiler::Benchmark
-      def parent_method = 'p'
+  context 'with benchmark_instance_methods in an inheritance hierarchy' do
+    let(:parent) do
+      Class.new do
+        include ClassProfiler::Benchmark
+        def parent_method = 'p'
+      end
     end
 
-    child = Class.new(parent) do
-      def child_method = 'c'
-      benchmark_instance_methods
+    let(:child) do
+      Class.new(parent) do
+        def child_method = 'c'
+        benchmark_instance_methods
+      end
     end
 
-    obj = child.new
-    obj.parent_method
-    obj.child_method
+    let(:obj) { child.new }
 
-    expect(obj.benchmarked).to include(:child_method)
-    expect(obj.benchmarked).not_to include(:parent_method)
+    before do
+      obj.parent_method
+      obj.child_method
+    end
+
+    it 'benchmarks only non-inherited methods' do
+      # parent method was invoked but should not be wrapped when selecting non-inherited
+      expect(benchmarked).not_to include(:parent_method)
+      expect(benchmarked).to include(:child_method)
+      expect(benchmarked[:child_method]).to be >= 0
+    end
   end
 
-  # NOTE: inherited-all behavior can vary with Ruby internals; covered by unit tests elsewhere
+  context 'with benchmark_all_methods including inherited methods' do
+    let(:parent) do
+      Class.new do
+        include ClassProfiler::Benchmark
+        def parent_method = 'p'
+        protected def parent_protected = 'pp'
+        private def parent_private = 'pv'
+      end
+    end
+
+    let(:child) do
+      Class.new(parent) do
+        def child_method = 'c'
+        protected def child_protected = 'cp'
+        private def child_private = 'cv'
+        benchmark_all_methods(visibility: :all)
+      end
+    end
+
+    let(:obj) { child.new }
+
+    it 'benchmarks inherited and child methods across visibilities' do
+      obj.parent_method
+      obj.send(:parent_protected)
+      obj.send(:parent_private)
+      obj.child_method
+      obj.send(:child_protected)
+      obj.send(:child_private)
+
+      expect(benchmarked).to include(:child_method, :parent_method, :child_protected, :parent_protected, :child_private, :parent_private)
+      expect(benchmarked.values).to all(be >= 0)
+    end
+  end
+
+  context 'with class method benchmarking' do
+    context 'explicit class methods' do
+      let(:klass) do
+        Class.new do
+          include ClassProfiler::Benchmark
+
+          def self.a = 1.+(1)
+          def self.b = sleep(0.002)
+
+          benchmark_class_methods :a, :b
+        end
+      end
+
+      it 'records timings for class methods' do
+        klass.a
+        klass.b
+        expect(klass.class_benchmarked).to include(:a, :b)
+        expect(klass.class_benchmarked[:a]).to be >= 0
+        expect(klass.class_benchmarked[:b]).to be >= 0
+      end
+    end
+
+    context 'own vs inherited class methods with visibility selection' do
+      let(:parent) do
+        Class.new do
+          include ClassProfiler::Benchmark
+          def self.p_pub = 'pp'
+          class << self
+            protected def p_prot = 'prot'
+            private def p_priv = 'priv'
+          end
+        end
+      end
+
+      let(:child) do
+        Class.new(parent) do
+          def self.c_pub = 'cp'
+          class << self
+            protected def c_prot = 'prot'
+            private def c_priv = 'priv'
+          end
+
+          benchmark_own_class_methods(visibility: :all)
+        end
+      end
+
+      it 'benchmarks only own class methods across visibilities' do
+        child.c_pub
+        child.send(:c_prot)
+        child.send(:c_priv)
+
+        expect(child.class_benchmarked).to include(:c_pub, :c_prot, :c_priv)
+        expect(child.class_benchmarked).not_to include(:p_pub, :p_prot, :p_priv)
+      end
+
+      it 'benchmarks inherited class methods when requested' do
+        child = Class.new(parent) do
+          def self.c_pub = 'cp'
+          benchmark_all_class_methods(visibility: :public)
+        end
+
+        child.p_pub
+        child.c_pub
+
+        expect(child.class_benchmarked).to include(:p_pub, :c_pub)
+      end
+    end
+  end
 end
